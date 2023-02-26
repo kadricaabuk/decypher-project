8614678bffe2c45befee15e962d531baf5f1ec82
kadricaabuk <kadricabuk@gmail.com>
2023-02-26 18:30:38 +0300

    header update

--- src/Layouts/DCPage.js
+++ src/Layouts/DCPage.js
@@ -4,8 +4,7 @@ const DCPage = ({children}) => {
   const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
-        backgroundColor: '#384151'
+        backgroundColor: '#384151',
       }
   })
   return (
--- src/Navigators/RootNavigator.js
+++ src/Navigators/RootNavigator.js
@@ -3,11 +3,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
+  const headerStyle = {
+    headerStyle: {
+      backgroundColor: '#384151',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'normal',
+    },
+  }
   return (
     <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
         <Stack.Screen name="Register" component={Register} options={{headerShown: false}}/>
-        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Home" component={Home} options={headerStyle}/>
     </Stack.Navigator>
   )
 }
